11c7cf9717703cadfd2eaa729a260de4807f92ca
Oscar Baracos <oscar.baracos@gmail.com>
2025-11-13 12:20:38 +0100

    Add stubs for helpers

---
+++ packages/token-frontend/src/helpers.ts
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/require-await */
+export async function getBalanceByInstrumentId(
+    owner: string,
+    instrumentId: string
+) {
+    // TODO: Implement actual logic
+    return 0;
+}
+
+export async function createTokenFactory(owner: string, instrumentId: string) {
+    return "contractId";
+}
+
+export async function getOrCreateTokenFactory(
+    owner: string,
+    instrumentId: string
+) {
+    return "contractId";
+}
+
+export async function transferToken(
+    to: string,
+    amount: number,
+    instrumentId: string
+) {
+    return "submissionId";
+}
