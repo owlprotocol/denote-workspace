7c89720dc0b4aa0e16f3dd6d75384b1019dfc1b4
Oscar Baracos <oscar.baracos@gmail.com>
2025-12-08 15:00:56 +0100

    Update minimal token package id

--- packages/token-sdk/src/uploadDars.ts
+++ packages/token-sdk/src/uploadDars.ts
@@ -22,7 +22,7 @@ export async function uploadDars() {
     // Obtained from runnning:
     // `pnpm get:minimal-token-id`
     const MINIMAL_TOKEN_PACKAGE_ID =
-        "72a07c72a9af38316e3f222a12888931f816cf22dabb9f22f3b474d966fea325";
+        "ad9c5643bbcc725d457dfca291a50fbca0c00c2ba6a7d4e8a8c89e8693550889";
 
     const isDarUploaded = await sdk.userLedger?.isPackageUploaded(
         MINIMAL_TOKEN_PACKAGE_ID
