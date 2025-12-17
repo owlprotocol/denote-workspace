90fd81cdde4acabef22b3f747bf2f4208b8fcd08
Oscar Baracos <oscar.baracos@gmail.com>
2025-12-17 18:27:11 +0100

    Implement etf burn, refactor token factory burn result

--- packages/token-sdk/src/constants/MINIMAL_TOKEN_PACKAGE_ID.ts
+++ packages/token-sdk/src/constants/MINIMAL_TOKEN_PACKAGE_ID.ts
@@ -1,4 +1,4 @@
 // Obtained from runnning:
 // `pnpm get:minimal-token-id`
 export const MINIMAL_TOKEN_PACKAGE_ID =
-    "194b49c286b92ac50948d93d26ae0b97188c6f91d98db92859880ce83468d0d5";
+    "df79b775ca53cf4edb3f80f1ed5077b79af27332b90360b60ef10a0247f69e2e";
